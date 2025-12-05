542f96302ae3c2b4c458a4943d0d0def1b8d0ea0
Ramy Cherrouk <ramy.cherrouk@epitech.eu>
2025-12-05 17:37:39 +0100

    ajout seeders et factories

---
+++ database/factories/ReservationFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Reservation>
+ */
+class ReservationFactory extends Factory
+{
+
+    protected $model = reservation::class;
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+
+        $start = $this->faker->dateTimeBetween('+1 days', '+1 month');
+        $end = (clone $start)->modify('+2 hours');
+        
+        return [
+             'user_id' => User::inRandomOrder()->first()->id ?? User::factory(),
+            'salle_id' => Salle::inRandomOrder()->first()->id ?? Salle::factory(),
+            'debut' => $start,
+            'fin' => $end,
+        ];
+    }
+}
